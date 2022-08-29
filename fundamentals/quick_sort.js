643498a60f0a750af63703388a544f863135b10c
Brian Lucius <brian@brianlucius.net>
2022-08-29 17:24:18 -0600

    Comment debug lines

--- fundamentals/quick_sort.js
+++ fundamentals/quick_sort.js
@@ -1,22 +1,22 @@
 const partition = (arr, i, j) => {
     const pivot = arr[Math.floor(Math.random()* (j - i) + i)];
     // pivot = 5;
-    console.log("i: ",i , " j: ", j, " Pivot Value: ", pivot);
+    // console.log("i: ",i , " j: ", j, " Pivot Value: ", pivot);
 
     while (i < j) {
         while (arr[i] < pivot) {
             i++;
-            console.log("i: ",i, " j:", j);
+            // console.log("i: ",i, " j:", j);
         } 
         while (arr[j] > pivot) {
             j--;
-            console.log("i: ",i, " j:", j);
+            // console.log("i: ",i, " j:", j);
         } 
         if (i <= j) {
             let temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
-            console.log(arr);
+            // console.log(arr);
             i++;
             j--;
             }
@@ -42,5 +42,5 @@ const arr2 = [1, 3, 5, 4, 8, 30, 20, 17, 7];
 
 // console.log("Sample: ", partition(sample, 0, 9));
 console.log("Sample: ", quickSort(sample));
-// console.log("Arr1: ", quickSort(arr1));
-// console.log("Arr2: ", quickSort(arr2));
+console.log("Arr1: ", quickSort(arr1));
+console.log("Arr2: ", quickSort(arr2));
