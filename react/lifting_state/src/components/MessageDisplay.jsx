2532d05ff1a27fbace201854504a9a3e3327b561
Brian Lucius <brian@brianlucius.net>
2022-09-01 18:55:25 -0600

    Add lifting state exercise

---
+++ react/lifting_state/src/components/MessageDisplay.jsx
@@ -0,0 +1,11 @@
+const MessageDisplay = (props) => {
+    return (
+        <>
+            <h1>Current Message</h1>
+            <pre>{ props.message }</pre>
+        </>
+    );
+};
+    
+export default MessageDisplay;
+
