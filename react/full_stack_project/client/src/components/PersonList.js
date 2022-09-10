9f72db63a8efa911ef2f64877a4b3393a051f1dc
Brian Lucius <brian@brianlucius.net>
2022-09-10 17:50:16 -0600

    Add functionality to full stack example

---
+++ react/full_stack_project/client/src/components/PersonList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+    
+const PersonList = (props) => {
+    return (
+        <div>
+            {props.people.map( (person, i) =>
+                <p key={i}>{person.lastName}, {person.firstName}</p>
+            )}
+        </div>
+    )
+}
+    
+export default PersonList;
+
