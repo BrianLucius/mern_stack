7bb9a638863450efc3c1df72c51d5ec087e0407b
Brian Lucius <brian@brianlucius.net>
2022-09-11 14:10:21 -0600

    Add edit and update to full stack demo

--- react/full_stack_project/client/src/App.js
+++ react/full_stack_project/client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Main from './views/Main';
 import Detail from './components/Detail';
+import Update from './views/Update';
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route element={<Main/>} path="/people/" />
           <Route element={<Detail/>} path="/people/:id" />
+          <Route element={<Update/>} path="/people/:id/edit"/>
         </Routes>
       </div>
     );
