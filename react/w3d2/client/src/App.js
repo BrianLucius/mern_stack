8d8daa4ba8240f1ef03af8a2a9d4e1ad842115a1
Brian Lucius <brian@brianlucius.net>
2022-09-13 15:15:51 -0600

    Add w3d2 afternoon lecture

--- react/w3d2/client/src/App.js
+++ react/w3d2/client/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';  
 import AllHeros from './components/AllHeros';
 import AddHero from './components/AddHero';
+import EditHero from './components/EditHero';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<AllHeros />} />
         <Route path="/hero/add_hero" element={<AddHero />} />
+        <Route path="/hero/edit/:id" element={<EditHero />} />
       </Routes>
     </div>
   );
--- react/w3d2/client/src/components/AllHeros.js
+++ react/w3d2/client/src/components/AllHeros.js
@@ -1,18 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const AllHeros = () => {
     const [allHeros, setAllHeros] = useState([]);
+    const [deleteToggle, setDeleteToggle] = useState(false);
 
     useEffect(() => {
         axios.get('http://localhost:8000/api/heros')
         .then((response) => {setAllHeros(response.data.results)})
         .catch((err) => {console.log("There was an error: ", err)});
-    }, []);
+    }, [deleteToggle]);
+
+    const deleteHero = (e, id) => {
+        console.log("Deleting hero id:", id);
+        axios.delete(`http://localhost:8000/api/hero/delete/${id}`)
+        .then((response) => {setDeleteToggle(!deleteToggle)})
+        .catch((err) => {console.log("There was an error: ", err)});
+    }
 
     return (
         <div className='container'>
             <h1>AllHeros</h1>
+            <Link to="/hero/add_hero">Add a Hero</Link>
             <table className='table'>
                 <thead>
                     <tr>
@@ -30,8 +40,8 @@ const AllHeros = () => {
                                     <td>{hero.name}</td>
                                     <td>{hero.rating}</td>
                                     <td><img src={hero.image} alt="Superhero" height="50"/></td>
-                                    <td><button className="btn btn-primary mx-1">Edit</button>
-                                        <button className="btn btn-danger mx-1">Delete</button></td>
+                                    <td><Link to={`/hero/edit/${hero._id}`} className="btn btn-primary mx-1">Edit</Link>
+                                        <button className="btn btn-danger mx-1" onClick={(e) => {deleteHero(e, hero._id)}}>Delete</button></td>
                                 </tr>
                             )
                         })
